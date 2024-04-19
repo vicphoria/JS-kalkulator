2bc14a9eb1eb8e889b187f0759ddb91f28c402a2
Lamija <vicunravel2@gmail.com>
2024-04-19 21:49:05 +0200

    Initial commit

---
+++ JS-kurs-kalkulator-main/assets/scripts/app.js
@@ -0,0 +1,51 @@
+alert("Zdravo");
+
+let pocetnaVrijednost = 0;
+
+pocetnaVrijednost = pocetnaVrijednost + 5;
+
+outputResult(pocetnaVrijednost, "0+5");
+
+
+function zbirBrojeva(a, b) {
+  let zbir = a + b;
+  return zbir;
+}
+
+zbirBrojeva(1, 3);
+
+
+
+function razlikaBrojeva(a, b) {
+  let razlika = a - b;
+  return razlika;
+}
+
+razlikaBrojeva(4, 3);
+
+
+
+function zbirBrojeva(a, b) {
+  let zbir = a + b;
+  return zbir;
+}
+
+ zbirBrojeva(1, 3);
+
+
+
+function proizvodBrojeva(a, b) {
+  let proizvod = a * b;
+  return proizvod;
+}
+
+proizvodBrojeva(2, 3);
+
+
+
+function kolicnikBrojeva(a, b) {
+  let kolicnik = a / b;
+  return kolicnik;
+}
+
+ kolicnikBrojeva(1, 3);
